40addc29db780d0358b5ae0624450bfc672acf4a
Harv38 <tush38635@gmail.com>
2021-06-24 18:24:02 +0530

    Indent

--- React/movies/src/Components/Movies.js
+++ React/movies/src/Components/Movies.js
@@ -1,53 +1,52 @@
 import React, { Component } from 'react'
 import { getMovies } from './getMovies';
 export default class Movies extends Component {
-    constructor()
-    {
+    constructor() {
         super();
-        this.state={
-            movies:getMovies()
+        this.state = {
+            movies: getMovies()
         }
     }
     render() {
         return (
             //JSX
             <div className='container'>
-           <div className='row'>
-               <div className='col-3'>
-                   Hello
-               </div>
-               <div className='col-9'>
-                   <table className="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Title</th>
-      <th scope="col">Genre</th>
-      <th scope="col">Stock</th>
-      <th scope="col">Rate</th>
-      <th></th>
-    </tr>
-  </thead>
-  <tbody>
-   {
-       this.state.movies.map((movieObj)=>{
-           return(
-               <tr scope='row' key={movieObj._id} >
-                   <td></td>
-                   <td>{movieObj.title}</td>
-                   <td>{movieObj.genre.name}</td>
-                   <td>{movieObj.numberInStock}</td>
-                   <td>{movieObj.dailyRentalRate}</td>
-                   <td><button type="button" className="btn btn-danger">Delete</button></td>
-               </tr>
-           )
-       })
-   }
-  </tbody>
-</table>
-               </div>
-           </div>
-           </div>
+                <div className='row'>
+                    <div className='col-3'>
+                        Hello
+                    </div>
+                    <div className='col-9'>
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Title</th>
+                                    <th scope="col">Genre</th>
+                                    <th scope="col">Stock</th>
+                                    <th scope="col">Rate</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    this.state.movies.map((movieObj) => {
+                                        return (
+                                            <tr scope='row' key={movieObj._id} >
+                                                <td></td>
+                                                <td>{movieObj.title}</td>
+                                                <td>{movieObj.genre.name}</td>
+                                                <td>{movieObj.numberInStock}</td>
+                                                <td>{movieObj.dailyRentalRate}</td>
+                                                <td><button type="button" className="btn btn-danger">Delete</button></td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
