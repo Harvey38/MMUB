8f4598f59d1db70b01e13aab89b0fdbed896addc
Harv38 <tush38635@gmail.com>
2021-06-24 18:46:17 +0530

    Delete

--- React/movies/src/Components/Movies.js
+++ React/movies/src/Components/Movies.js
@@ -7,6 +7,15 @@ export default class Movies extends Component {
             movies: getMovies()
         }
     }
+    onDelete=(id)=>{
+        let arr =this.state.movies.filter(function(movieObj){
+            return movieObj._id!=id;
+        })
+        // console.log(arr);
+        this.setState({
+            movies:arr
+        });
+    }
     render() {
         return (
             //JSX
@@ -31,13 +40,15 @@ export default class Movies extends Component {
                                 {
                                     this.state.movies.map((movieObj) => {
                                         return (
-                                            <tr scope='row' key={movieObj._id} >
+                                            <tr key={movieObj._id} >
                                                 <td></td>
                                                 <td>{movieObj.title}</td>
                                                 <td>{movieObj.genre.name}</td>
                                                 <td>{movieObj.numberInStock}</td>
                                                 <td>{movieObj.dailyRentalRate}</td>
-                                                <td><button type="button" className="btn btn-danger">Delete</button></td>
+                                                <td><button onClick={function(){
+                                                    this.onDelete(movieObj._id)
+                                                }.bind(this)} type="button" className="btn btn-danger">Delete</button></td>
                                             </tr>
                                         )
                                     })
