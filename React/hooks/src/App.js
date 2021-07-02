972544cbf904287feddf22d38736fedd80e342a2
Harv38 <tush38635@gmail.com>
2021-07-02 19:07:23 +0530

    Ue2

--- React/hooks/src/App.js
+++ React/hooks/src/App.js
@@ -1,11 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
 import Us from './UseState/Us';
+import Ue2 from './useEffect/Ue2';
 function App() {
   return (
 <>
 {/* <h1>Heelo</h1> */}
-<Us/>
+{/* <Us/>
+ */}
+<Ue2/>
 </>
   );
 }
