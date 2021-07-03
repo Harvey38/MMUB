51684f85bfdd212a70543035986a255e1b189186
Harv38 <tush38635@gmail.com>
2021-07-03 14:22:37 +0530

    firebase

---
+++ React/firebase/src/Components/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+firebase.initializeApp(
+{
+    apiKey: "AIzaSyCCzVQChFWRV9jfOcBWfEFFNJ-GFi0t_Mg",
+    authDomain: "class-demo-3aad1.firebaseapp.com",
+    projectId: "class-demo-3aad1",
+    storageBucket: "class-demo-3aad1.appspot.com",
+    messagingSenderId: "627494549770",
+    appId: "1:627494549770:web:10ede4b310f2c1a51d22f3"
+  })
+
+  export default firebase;
