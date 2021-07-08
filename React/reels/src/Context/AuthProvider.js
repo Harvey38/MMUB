12f99b485e5217dfa89d5d91153d75ff847854fd
Harv38 <tush38635@gmail.com>
2021-07-08 20:11:48 +0530

    authContext

---
+++ React/reels/src/Context/AuthProvider.js
@@ -0,0 +1,41 @@
+import React,{useState,useContext,useEffect} from 'react'
+import {auth} from '../firebase'
+const AuthContext = React.createContext();
+function AuthProvider({children}) {
+    const[currentUser,setCurrentUser] =useState();
+    const[loading,setLoading] =useState(true);
+    function signup(email,password)
+    {
+        return auth.createUserWithEmailAndPassword(email,password);
+    }
+    function login(email,password)
+    {
+        return auth.signInWithEmailAndPassword(email,password);
+    }
+    function logout()
+    {
+        return auth.signOut();
+    }
+    useEffect(()=>{
+        const unsubscribe  = auth.onAuthStateChanged(user=>{
+            setCurrentUser(user);
+            setLoading(false);
+        })
+        return ()=>{
+            unsubscribe();
+        }
+    },[])
+    const value = {
+        currentUser,
+        login,
+        signup,
+        logout
+    }
+    return (
+        <AuthContext.Provider value={value}>
+            {!loading&&children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthProvider
---
+++ React/reels/src/firebase.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app"
+import "firebase/auth"
+import 'firebase/storage'
+import 'firebase/firestore'
+
+firebase.initializeApp(
+    {
+        apiKey: "AIzaSyC3lA1kH4CyB7NqJH67WDRsNRw277Nsd74",
+        authDomain: "reels-class.firebaseapp.com",
+        projectId: "reels-class",
+        storageBucket: "reels-class.appspot.com",
+        messagingSenderId: "176437527266",
+        appId: "1:176437527266:web:bf358df480bfd9302214bb"
+      }
+)
+export const auth = firebase.auth();
+const firestore = firebase.firestore();
+export const database ={
+    users:firestore.collection('users'),
+    getCurrentTimeStamp : firebase.firestore.FieldValue.serverTimestamp
+}
+export const storage = firebase.storage();
+// export default firebase;
