4182789d1e3c1ac26d6cc0391ff97cb6b5f8c628
Harv38 <tush38635@gmail.com>
2021-07-17 01:14:15 +0530

    css change

--- React/reels/src/Components/Feed.js
+++ React/reels/src/Components/Feed.js
@@ -18,7 +18,7 @@ function Feed() {
         <>
         { userData==null ? <CircularProgress />:<>
         <Header userData={userData} />
-        <div style={{height:'9.5vh'}}/>
+        <div style={{height:'1.5vh'}}/>
         <div className='feed-container'>
             <div className='center'>
                 <UploadFile userData={userData}/>
