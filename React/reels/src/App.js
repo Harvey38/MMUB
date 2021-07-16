998c4b085672ca20cc5b6c8fbf8b6721c16ce417
Harv38 <tush38635@gmail.com>
2021-07-16 19:20:42 +0530

    remove components.rar

--- React/reels/src/App.js
+++ React/reels/src/App.js
@@ -4,13 +4,15 @@ import Signup from './Components/Signup';
 import AuthProvider from './Context/AuthProvider';
 import Main from './MaterialUI/Main';
 import Login from './Components/Login';
+import Ioa from './Components/Ioa';
 function App() {
   return (
-    <AuthProvider>
-    <Signup/>
-    {/* <Login/> */}
-    </AuthProvider>
-    // <Main/>
+    // <AuthProvider>
+    // <Signup/>
+    // {/* <Login/> */}
+    // </AuthProvider>
+    // // <Main/>
+    <Ioa/>
   );
 }
 
